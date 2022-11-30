ae49a03caf83362a8650b7c5e24aba15087b466f
CristopherArroba <caarroba@espol.edu.ec>
2022-11-29 19:17:42 -0500

    varios

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module ec.edu.espol.proyectoestructuradedatos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.proyectoestructuradedatos to javafx.fxml;
-    opens ec.edu.espol.model to javafx.fxml;
+    opens ec.edu.espol.controller to javafx.fxml;
     exports ec.edu.espol.proyectoestructuradedatos;
-    exports ec.edu.espol.model;
+    exports ec.edu.espol.controller;
 }
