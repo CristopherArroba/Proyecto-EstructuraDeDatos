f54c23ded630b19254d529e9627ee14027b29e6e
CristopherArroba <caarroba@espol.edu.ec>
2022-11-30 18:59:43 -0500

    PrincipalController

--- src/main/java/ec/edu/espol/controller/PrincipalController.java
+++ src/main/java/ec/edu/espol/controller/PrincipalController.java
@@ -4,11 +4,15 @@
  */
 package ec.edu.espol.controller;
 
+import ec.edu.espol.model.Juego;
+import ec.edu.espol.util.CircularDoubleLinkedList;
 import java.net.URL;
+import java.util.ListIterator;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -25,21 +29,41 @@ public class PrincipalController implements Initializable {
     private Button siguiente;
     @FXML
     private ImageView imv;
-
+    
+    private CircularDoubleLinkedList<Juego> juegos = Juego.leerJuegos("juegos.ser");
+    private ListIterator<Juego> lit = juegos.listIterator();
+    //private Node<Juego> nodo = juegos.getNode(0);
+    Juego j = lit.next();
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        //CircularDoubleLinkedList<Juego> juegos = Juego.leerJuegos("juegos.ser");
+//        for(int k = 0; k<juegos.size(); k++){
+//            Juego j = juegos.get(k);
+//            Image i = new Image("img."+juegos.get(k).getTitulo()+"/"+juegos.get(k).getTitulo()+"-cap1");
+//            imv.setImage(i);
+        Image i = new Image("img."+j.getTitulo()+"/"+j.getTitulo()+"-cap1");
+        imv.setImage(i);
+            
+        
+        
     }    
 
     @FXML
     private void retroceder(MouseEvent event) {
+        j = lit.previous();
+        Image i = new Image("img."+j.getTitulo()+"/"+j.getTitulo()+"-cap1");
+        imv.setImage(i);
     }
 
     @FXML
     private void avanzar(MouseEvent event) {
+        j = lit.next();
+        Image i = new Image("img."+j.getTitulo()+"/"+j.getTitulo()+"-cap1");
+        imv.setImage(i);
     }
 
     @FXML
